549924d00d9cf92e979eac32d549498bb8a97ccf
Shane <6071159+smashedr@users.noreply.github.com>
2024-04-26 00:22:00 -0700

    Update Screenshots

--- js/uninstall.js
+++ js/uninstall.js
@@ -14,43 +14,42 @@ uninstallForm.addEventListener('submit', formSubmit)
 function formSubmit(event) {
     console.debug('formSubmit:', event, this)
     event.preventDefault()
-    // this[0] switch   - not needed or used
-    // this[1] switch   - not working as expected
-    // this[2] textarea - feedback description
-    // this[3] input    - discord webhook url
-    // this[4] button   - submit
-    const url = this[3].value
-    if (!(this[0].checked || this[1].checked || this[2].value)) {
-        console.warn('No Data to Send.')
-    } else {
-        submitBtn.classList.add('disabled')
-        const lines = [
-            'Uninstall Feedback.',
-            `Not Used: **${this[0].checked}**`,
-            `Not Working: **${this[1].checked}**`,
-            '```\n' + `${this[2].value || 'No Reason Provided.'}` + '\n```',
-        ]
-        const xhr = new XMLHttpRequest()
-        xhr.open('POST', url)
-        xhr.setRequestHeader('Content-type', 'application/json')
-        const params = {
-            username: 'PlayDrift Extension',
-            avatar_url: 'https://playdrift-extension.cssnr.com/images/logo.png',
-            content: lines.join('\n'),
-        }
-        xhr.onload = () => {
-            console.debug('xhr.status: ', xhr.status)
-            submitBtn.classList.remove('disabled')
-            if (xhr.status >= 200 && xhr.status <= 299) {
-                console.debug('SUCCESS')
-                window.location = '/'
-            } else {
-                console.log(`ERROR: ${xhr.status}`)
-                const errorEl = document.getElementById('error')
-                errorEl.textContent = `Submission Error: ${xhr.status}`
-                errorEl.style.display = 'block'
-            }
-        }
-        xhr.send(JSON.stringify(params))
+    const notUsed = this[0].checked
+    const notExpected = this[1].checked
+    const notWorking = this[2].checked
+    const feedbackText = this[3].value
+    const url = this[4].value
+    if (!(notUsed || notExpected || notWorking || feedbackText)) {
+        return console.warn('No Data to Send.')
     }
+    submitBtn.classList.add('disabled')
+    const lines = [
+        'Uninstall Feedback.',
+        `Not Used: **${notUsed}**`,
+        `Not as Expected: **${notExpected}**`,
+        `Not Working: **${notWorking}**`,
+        '```\n' + `${feedbackText || 'No Reason Provided.'}` + '\n```',
+    ]
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', url)
+    xhr.setRequestHeader('Content-type', 'application/json')
+    const params = {
+        username: 'PlayDrift Extension',
+        avatar_url: 'https://playdrift-extension.cssnr.com/images/logo.png',
+        content: lines.join('\n'),
+    }
+    xhr.onload = () => {
+        console.debug('xhr.status: ', xhr.status)
+        submitBtn.classList.remove('disabled')
+        if (xhr.status >= 200 && xhr.status <= 299) {
+            console.debug('SUCCESS')
+            window.location = '/'
+        } else {
+            console.log(`ERROR: ${xhr.status}`)
+            const errorEl = document.getElementById('error')
+            errorEl.textContent = `Submission Error: ${xhr.status}`
+            errorEl.style.display = 'block'
+        }
+    }
+    xhr.send(JSON.stringify(params))
 }
