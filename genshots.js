b2dfe2841b0a4962392dd62983a2a3da6458968d
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2024-06-15 12:27:52 -0700

    Move to src (#5)
    * Move to src
    
    * Fix

--- genshots.js
+++ genshots.js
@@ -1,19 +1,20 @@
-const baseDir = './screenshots'
+const repoDir = './src/screenshots'
+const relPath = ''
 
 const fs = require('fs')
 const files = []
-fs.readdirSync(baseDir).forEach((file) => {
+fs.readdirSync(repoDir).forEach((file) => {
     files.push(file)
 })
 console.log(files)
 
-const file = fs.createWriteStream('./js/vars.js')
+const file = fs.createWriteStream('./src/js/vars.js')
 file.on('error', function (err) {
     console.error('err', err)
 })
 file.write('const screenShots = [\n')
 files.forEach(function (x) {
-    file.write(`    '${x}',\n`)
+    file.write(`    '${relPath}${x}',\n`)
 })
 file.write(']\n')
 file.end()
